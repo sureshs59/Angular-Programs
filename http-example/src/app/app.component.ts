52e179cdaaea517c8eb9f44c1c246965682b8479
suresh sunuguri <sunuguri.suresh@gmail.com>
2026-04-28 10:05:02 -0400

    Http-Observable example

---
+++ http-example/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { User, UserService } from './services/user.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'http-example';
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+  users: User[] = [];
+  loading = false;
+  errorMessage = '';
+
+  loadUsers(): void {
+    this.loading = true;
+    this.errorMessage = '';   
+  
+    this.userService.getUsers().subscribe({
+      next: (users) => {
+        this.users = users;
+        this.loading = false;
+      },
+      error: (error) => {
+        this.errorMessage = 'Error fetching users';
+        this.loading = false;
+      }
+    });
+  }
+}
